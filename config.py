28f90832f1a737c430a2f366b69c8602d9d56951
Erovhost <owlsarenot@gmail.com>
2026-09-23 13:52:04 +0300

    Добавлен config.py: настройки читаются из .env

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+# Папка, в которой лежит этот файл (корень проекта)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Загружаем переменные из .env в переменные окружения
+load_dotenv(os.path.join(basedir, ".env"))
+
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
